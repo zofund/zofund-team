ca2c1df08bd596d4347f146b65fe1f7c53f43240
zhouzongjin <you@example.com1356782505@qq.com>
2020-07-09 13:49:51 +0800

    加入表合并选项

--- final_version/src/DataTrain/HowNet_SOPMI.py
+++ final_version/src/DataTrain/HowNet_SOPMI.py
@@ -475,11 +475,13 @@ class HowNetModel:
             score_grade = {}
             # 定位句子
 
-            achievement_sentence = self.word_location(sentenses, ['业绩'])
-            degree_of_boom_sentence = self.word_location(sentenses, ['行业景气度','量价','产业','行业'])
-            market_share_sentence = self.word_location(sentenses, ['市场占有率','体量','规模','集中度','市占率','市场份额','占率'])
-            price_sentence = self.word_location(sentenses, ['产品价格','价格','批价','出厂价','量价'])
-            transformation_sentence = self.word_location(sentenses, ['转型','转变'])
+            achievement_sentence = self.word_location(sentenses, ['业绩','亮眼','靓丽','超额完成'])
+            degree_of_boom_sentence = self.word_location(sentenses, ['行业景气度','量价','产业','行业','景气',])
+            market_share_sentence = self.word_location(sentenses, ['市场占有率','体量','规模','集中度','市占率',
+                                                                   '市场份额','占率','渗透率','扩张','拓展','拓张',
+                                                                   '扩展','并购','收购','开拓市场',''])
+            price_sentence = self.word_location(sentenses, ['产品价格','价格','批价','出厂价','量价','涨价','提价'])
+            transformation_sentence = self.word_location(sentenses, ['转型','转变','转型期','变革','外延','重组','整合'])
 
             print('--------')
             print(i)
